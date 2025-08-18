fce285b020f499ebf0a262d66398944d275dfd80
Makarov Sergey <makarov.s.v2000@yandex.ru>
2025-08-18 16:21:28 +0300

    refactor: fixed delay timer

--- drivers/core/timer.c
+++ drivers/core/timer.c
@@ -2,7 +2,7 @@
 #include "memory_map.h"
 #include "rcc.h"
 
-void timer_init()
+void init_timer()
 {
     RCC_Frequencies rcc_clocks = {0};
     RCC_BusConfig rcc_config = {0};
@@ -14,15 +14,18 @@ void timer_init()
     uint32_t timer_clk = rcc_clocks.APB1_Freq;
     if (rcc_config.apb1_prescaler > 1)
         timer_clk *= 2;
-    timer->PSC = (timer_clk / 1000) - 1; // 1 тик = 1 мс
-    timer->CR1 |= TIMER_CR1_ARPE;                   // авто-перезагрузка
+    timer->PSC = (timer_clk / 1000) - 1;
+    timer->CR1 |= TIMER_CR1_ARPE;
+    timer->EGR = TIMER_EGR_UG;
 }
 
 void delay_timer(uint32_t ms)
 {
     GP_Timer_Type *timer = (GP_Timer_Type *)TIM2_REG;
-    timer->ARR = ms - 1;
+    timer->CR1 &= ~TIMER_CR1_CEN;
+    timer->ARR = (ms ? ms - 1 : 0);
     timer->CNT = 0;
+    timer->EGR = TIMER_EGR_UG;
     timer->SR = 0;
 
     timer->CR1 |= TIMER_CR1_CEN;
@@ -30,6 +33,7 @@ void delay_timer(uint32_t ms)
     while (!(timer->SR & TIMER_SR_UIF))
         ;
 
-    timer->SR &= ~TIMER_SR_UIF;
+
     timer->CR1 &= ~TIMER_CR1_CEN;
+    timer->SR = 0;
 }
